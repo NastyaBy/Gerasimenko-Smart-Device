c76a2638913c85bff8b68a200f7eb8000a673e81
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-06-04 16:39:57 +0300

    PP

--- source/js/main.js
+++ source/js/main.js
@@ -26,40 +26,43 @@ accordionHeading.forEach(function (item) {
 // валидации телефона
 function setCursorPosition(pos, e) {
   e.focus();
-  if (e.setSelectionRange) e.setSelectionRange(pos, pos);
-  else if (e.createTextRange) {
+  if (e.setSelectionRange) {
+    e.setSelectionRange(pos, pos);
+  } else if (e.createTextRange) {
     var range = e.createTextRange();
     range.collapse(true);
-    range.moveEnd("character", pos);
-    range.moveStart("character", pos);
-    range.select()
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
   }
 }
 
 function mask(e) {
-  //console.log('mask',e);
-  var matrix = '+7 (___) ___-__-__',// .defaultValue
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
-    val = this.value.replace(/\D/g, "");
+  var self = e.target;
+  var matrix = '+7 (___) ___-__-__';
+  var i = 0;
+  var def = matrix.replace(/\D/g, '');
+  var val = self.value.replace(/\D/g, '');
   def.length >= val.length && (val = def);
-  matrix = matrix.replace(/[_\d]/g, function(a) {
-    return val.charAt(i++) || "_"
+  matrix = matrix.replace(/[_\d]/g, function () {
+    return val.charAt(i++) || '_';
   });
-  this.value = matrix;
+  self.value = matrix;
   i = matrix.lastIndexOf(val.substr(-1));
-  i < matrix.length && matrix != this.placeholder ? i++ : i = matrix.indexOf("_");
-  setCursorPosition(i, this)
+  i < matrix.length && matrix !== self.placeholder ? i++ : i = matrix.indexOf('_');
+  setCursorPosition(i, self);
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-  var inputsTel = document.querySelectorAll(".js-validateTel");
+window.addEventListener('DOMContentLoaded', function () {
+  var inputsTel = document.querySelectorAll('.js-validateTel');
 
-  inputsTel.forEach( function (input) {
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", setCursorPosition(3, input), false);
-    }
-  )
+  inputsTel.forEach(function (input) {
+    input.addEventListener('input', mask, false);
+    input.addEventListener('click', function (e) {
+      mask(e);
+      setCursorPosition(4, e.target);
+    }, false);
+  });
 });
 
 // валидации имени
@@ -78,29 +81,13 @@ if (inputNames) {
   });
 }
 
-// активация кнопки отправки
-var forms = document.querySelectorAll('.js-form');
-
-if (forms) {
-  forms.forEach(function (form) {
-    var accord = form.querySelector('.js-accord');
-    var sendForm = form.querySelector('.js-sendForm');
-
-    if (accord) {
-      accord.addEventListener('change', function (e) {
-        sendForm.disabled = !e.target.checked;
-      });
-    }
-  });
-}
-
 // отправка формы
 var sendForms = document.querySelectorAll('.js-sendForm');
 
 if (sendForms) {
   sendForms.forEach(function (button) {
     button.addEventListener('click', function () {
-      var form = button.form
+      var form = button.form;
       var inputName = form.querySelector('.js-validateName');
       var inputTel = form.querySelector('.js-validateTel');
       var textareaComment = form.querySelector('.js-comment');
@@ -118,30 +105,19 @@ if (sendForms) {
 }
 
 // открытие модального окна
-const modalTriggers = document.querySelectorAll('.js-modalOpen');
-const bodyBlackout = document.querySelector('.js-modalBlackout');
-const modalCloseBtn = document.querySelector('.js-modalCloseBtn');
+var modalTriggers = document.querySelectorAll('.js-modalOpen');
+var bodyBlackout = document.querySelector('.js-modalBlackout');
+var modalCloseBtn = document.querySelector('.js-modalCloseBtn');
 
-const clearFormErrors = () => {
-  const errorMessages = Array.prototype.slice.call(
-    document.querySelectorAll('.form-input__error'));
-
-  errorMessages.forEach( function (errorMessage) {
-    errorMessage.parentNode.removeChild(errorMessage);
-  });
-};
-
-const closeModal = function (popup) {
+var closeModal = function (popup) {
   popup.classList.remove('is--visible');
   bodyBlackout.classList.remove('is-blacked-out');
-
-  clearFormErrors()
-}
+};
 
 modalTriggers.forEach(function (trigger) {
   trigger.addEventListener('click', function (evt) {
-    const popupTrigger = trigger.dataset.popupTrigger;
-    const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`);
+    var popupTrigger = trigger.dataset.popupTrigger;
+    var popupModal = document.querySelector('[data-popup-modal=' + popupTrigger + ']');
 
     evt.preventDefault();
     popupModal.classList.add('is--visible');
@@ -154,10 +130,10 @@ modalTriggers.forEach(function (trigger) {
     modalCloseBtn.addEventListener('click', function () {
       closeModal(popupModal);
     });
-    window.addEventListener('keydown', function (evt) {
-      if (evt.keyCode === 27) {
+    window.addEventListener('keydown', function (e) {
+      if (e.keyCode === 27) {
         if (popupModal.classList.contains('is--visible')) {
-          evt.preventDefault();
+          e.preventDefault();
           closeModal(popupModal);
         }
       }
